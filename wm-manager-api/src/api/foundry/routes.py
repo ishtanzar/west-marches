002f77ea453e5a83e8496a5418f4a165aa5f16a5
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2021-09-11 08:15:40 +0200

    feat(users): create & update users

--- wm-manager-api/src/api/foundry/routes.py
+++ wm-manager-api/src/api/foundry/routes.py
@@ -1,6 +1,7 @@
 from compose.project import NoSuchService
 
 from api import WestMarchesApi
+from quart import request
 
 app = WestMarchesApi.instance
 
@@ -16,8 +17,30 @@ async def restart(user):
     return 'Done', 204
 
 
-@app.route('/foundry/roster')
-async def foundry_roster():
+@app.route('/foundry/actors')
+async def foundry_actors():
+    return app.foundryvtt.find_actors()
+
+
+@app.route('/foundry/users', methods=['POST'])
+@app.auth.required
+async def foundry_users_add(user):
+    json_request = await request.json
+    name = json_request['name']
+    user_id = app.foundryvtt.add_user(name)
+
     return {
-        'heroes': app.foundryvtt.find_actors()
+        'user_id': user_id
     }
+
+
+@app.route('/foundry/users/<user_id>', methods=['PUT'])
+async def foundry_users_update(user, user_id):
+    json_request = await request.json
+
+    name = json_request['name'] if 'name' in json_request else None
+    role = json_request['role'] if 'role' in json_request else None
+
+    app.foundryvtt.update_user(user_id, name, role)
+
+    return 'Done', 204
--- wm-manager-api/src/services/foundryvtt.py
+++ wm-manager-api/src/services/foundryvtt.py
@@ -1,9 +1,14 @@
 import json
-import logging
+from typing import Optional
 
+import requests
 import smart_open
 
 from utils import get_logger
+
+
+class FoundryApiException:
+    pass
 
 
 class FoundryService:
@@ -13,6 +18,25 @@ class FoundryService:
         self._endpoint = endpoint
 
     def find_actors(self):
-        with smart_open.open('%s/actors' % self._endpoint) as fp:
-            roster: dict = json.load(fp)
-            return roster['actors']
+        with smart_open.open('%s/api/actors' % self._endpoint) as fp:
+            return json.load(fp)
+
+    def add_user(self, name: str):
+        resp: requests.Response = requests.post('%s/api/users' % self._endpoint, json={
+            'name': name
+        })
+
+        if resp.status_code >= 400:
+            self.log.warning('Failed to create user', extra={'status_code': resp.status_code, 'response': resp})
+
+        resp_json = resp.json()
+        return resp_json['user_id']
+
+    def update_user(self, user_id: str, name: Optional[str] = None, role: Optional[int] = None):
+        resp: requests.Response = requests.put('%s/api/users/%s' % (self._endpoint, user_id), json={
+            'name': name,
+            'role': role
+        })
+
+        if resp.status_code >= 400:
+            self.log.warning('Failed to update user', extra={'status_code': resp.status_code, 'response': resp})
