d0827c1d773e806a5d8067b97e2ad4e1a0a1a7d8
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2021-09-13 14:47:06 +0200

    chore: remove discord & kanka derelicts

--- wm-foundry-module/scripts/init.js
+++ wm-foundry-module/scripts/init.js
@@ -1,9 +1,5 @@
 console.log("WestMarches | Initialising");
 
 import {WestMarchesLayer} from './controls.js';
-import {registerSettings} from "./discord.js";
-import {Kanka} from './kanka.js';
 
-registerSettings();
 WestMarchesLayer.initialize();
-Kanka.initialize();
