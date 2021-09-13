96e1d50b20d538955694218d50191ea64c61797c
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2021-09-13 14:48:26 +0200

    feat(overrides): override User.getUsers so the custom schema is sent to the UI

--- foundry-extensible-plugin/override/dist/user.js
+++ foundry-extensible-plugin/override/dist/user.js
@@ -19,6 +19,35 @@ class ExtensibleUser extends User {
     return schema;
   }
 
+  static async getUsers() {
+    const {game} = global;
+    const CONST = require(paths.code + '/const');
+
+    const users = await this.find({});
+    this.updateActivity(users);
+
+    users.sort((user1, user2) => {
+      let role1 = user1.role >= CONST.USER_ROLES['ASSISTANT'] ? user1['role'] : 1;
+      let role2 = user2['role'] >= CONST.USER_ROLES['ASSISTANT'] ? user2.role : 1;
+      if (role1 !== role2) return role2 - role1;
+      return user1.name.localeCompare(user2.name);
+    });
+
+    return game.users = users;
+  }
+
+  static updateActivity(users) {
+    const {game} = global;
+    const connectedUsers = game.activity.users;
+
+    for (let user of users) {
+      if (!user['_id']) continue;
+      const connected = connectedUsers[user['_id']] || {};
+      user['active'] = connected['active'] || false;
+      user['sceneId'] = connected['sceneId'] || null;
+    }
+  }
+
 }
 
 exports.default = ExtensibleUser;
