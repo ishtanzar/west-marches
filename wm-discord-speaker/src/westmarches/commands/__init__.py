888b3f02ac7ef8a0f97383a4a7c5caa5a8296c0c
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2021-11-11 08:09:19 +0100

    feat(commands): add gm command to send a message to all GMs

--- wm-discord-speaker/src/westmarches/commands/__init__.py
+++ wm-discord-speaker/src/westmarches/commands/__init__.py
@@ -1,5 +1,6 @@
 from .agenda import AgendaCommands
 from .chatbot import ChatbotCommands
+from .forward import Forward
 from .foundryvtt import FoundryCommands
 from .rumors import RumorsCommands
 from ..utils import CompositeMetaClass
@@ -9,5 +10,6 @@ class Commands(RumorsCommands,
                ChatbotCommands,
                FoundryCommands,
                AgendaCommands,
+               Forward,
                metaclass=CompositeMetaClass):
     pass
---
+++ wm-discord-speaker/src/westmarches/commands/forward.py
@@ -0,0 +1,56 @@
+import discord
+from redbot.core import commands, checks
+
+from westmarches.utils import CompositeMetaClass, MixinMeta
+
+
+class Forward(MixinMeta, metaclass=CompositeMetaClass):
+
+    @staticmethod
+    def _append_attachements(message: discord.Message, embeds: list):
+        attachments_urls = []
+        for attachment in message.attachments:
+            if any(attachment.filename.endswith(imageext) for imageext in ["jpg", "png", "gif"]):
+                if embeds[0].image:
+                    embed = discord.Embed()
+                    embed.set_image(url=attachment.url)
+                    embeds.append(embed)
+                else:
+                    embeds[0].set_image(url=attachment.url)
+            else:
+                attachments_urls.append(f"[{attachment.filename}]({attachment.url})")
+        if attachments_urls:
+            embeds[0].add_field(name="Attachments", value="\n".join(attachments_urls))
+        return embeds
+
+    async def _destination(self, msg: str = None, embed: discord.Embed = None):
+        await self.bot.wait_until_ready()
+        channel = await self.config.destination()
+        channel = self.bot.get_channel(channel)
+        if channel is None:
+            await self.bot.send_to_owners(msg, embed=embed)
+        else:
+            await channel.send(msg, embed=embed)
+
+    @commands.command()
+    async def gm(self, ctx: commands.Context, *message):
+        embeds = [discord.Embed(description=' '.join(message))]
+        embeds[0].set_author(name=f"{ctx.message.author} | {ctx.message.author.id}", icon_url=ctx.message.author.avatar_url)
+        embeds = self._append_attachements(ctx.message, embeds)
+        embeds[-1].timestamp = ctx.message.created_at
+        for embed in embeds:
+            await self._destination(msg=None, embed=embed)
+
+    @checks.has_permissions(administrator=True)
+    @commands.command()
+    async def gm_setforward(self, ctx, channel: discord.TextChannel = None):
+        """Set if you want to receive notifications in a channel instead of your DMs.
+        Leave blank if you want to set back to your DMs.
+        """
+        data = (
+            {"msg": "Notifications will be sent in your DMs.", "config": None}
+            if channel is None
+            else {"msg": f"Notifications will be sent in {channel.mention}.", "config": channel.id}
+        )
+        await self.config.destination.set(data["config"])
+        await ctx.send(data["msg"])
