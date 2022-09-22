1b517d04161330c7f5bd85e76075dc84a2862c3d
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2022-09-22 21:44:24 +0200

    feat(websocket): connect to websocket on cog setup

--- wm-discord-speaker/pm2.config.js
+++ wm-discord-speaker/pm2.config.js
@@ -5,10 +5,11 @@ module.exports = {
   apps : [{
     name: "discordbot",
     interpreter: "/usr/bin/python3",
-    interpreter_args : "-m debugpy --listen 0.0.0.0:5679",
+    // interpreter_args : "-m debugpy --listen 0.0.0.0:5679", // VScode
+    interpreter_args : "/usr/local/lib/python3.8/dist-packages/pydevd.py --multiprocess --port 5679 --client 172.17.0.1 --file", // PyCharm
     script : "/usr/local/bin/redbot",
     args : `westmarches --mentionable --token=${DISCORD_BOT_SECRET} --prefix=YVwWFZyjiFQH --owner=${DISCORD_OWNER_ID}`,
-    ignore_watch: ["data"],
+    ignore_watch: ['data', '/opt/project/wm-discord-speaker/src/westmarches/__pycache__'],
     watch: "/opt/project/wm-discord-speaker/src"
   }]
 }
