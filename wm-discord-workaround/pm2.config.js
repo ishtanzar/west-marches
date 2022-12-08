3c88680118a54312afbd661401a4d5bd7531040f
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2022-12-08 23:48:40 +0100

    feat: keep question threads opened + handle the "Playing" role

--- wm-discord-workaround/pm2.config.js
+++ wm-discord-workaround/pm2.config.js
@@ -5,7 +5,7 @@ module.exports = {
         name: "discordbot",
         interpreter: "python",
         // interpreter_args : "-m debugpy --listen 0.0.0.0:5679", // VScode
-        // interpreter_args: "/usr/local/lib/python3.8/dist-packages/pydevd.py --multiprocess --port 5679 --client 172.17.0.1 --file", // PyCharm
+        // interpreter_args: "/usr/local/lib/python3.10/site-packages/pydevd.py --multiprocess --port 5679 --client 172.17.0.1 --file", // PyCharm
         script: "/opt/project/wm-discord-workaround/src/__main__.py",
         ignore_watch: [],
         watch: "/opt/project/wm-discord-workaround/src"
--- wm-discord-workaround/src/__main__.py
+++ wm-discord-workaround/src/__main__.py
@@ -1,4 +1,5 @@
 import json
+import logging.config
 import os
 from pathlib import Path
 from types import SimpleNamespace
@@ -42,21 +43,25 @@ class PlayerReportConfirm(discord.ui.View):
 
 class WorkaroundCog(commands.Cog):
 
-    def __init__(self, config: Config) -> None:
+    def __init__(self, config: Config, bot: commands.Bot) -> None:
         self._config = config
+        self._bot = bot
+        self._logger = logging.getLogger('cog')
 
     @staticmethod
-    async def get_thread_original_message_mentions(channel: discord.Thread, guild: discord.Guild):
-        members = []
-
+    async def get_thread_original_message(channel: discord.Thread):
         async for starter_message in channel.history(limit=1, oldest_first=True):
             if starter_message.type is discord.MessageType.thread_starter_message:
-                original_message = starter_message.reference.resolved
+                return starter_message.reference.resolved
 
-                for user in original_message.mentions:
-                    members.append((await guild.query_members(user_ids=[user.id]))[0])
+    async def get_thread_original_message_mentions(self, channel: discord.Thread, guild: discord.Guild):
+        members = []
+        original_message = await self.get_thread_original_message_mentions(channel)
 
-                return members
+        for user in original_message.mentions:
+            members.append((await guild.query_members(user_ids=[user.id]))[0])
+
+        return members
 
     @commands.command(name="session")
     async def session_start(self, ctx: commands.Context):
@@ -91,6 +96,58 @@ class WorkaroundCog(commands.Cog):
         else:
             pass
 
+    async def on_raw_thread_update(self, payload: discord.RawThreadUpdateEvent):
+        if payload.parent_id == self._config.discord.questions_channel and payload.thread and payload.thread.archived:
+            self._logger.info(f"Thread '{payload.thread.name}' is archiving, checking for status")
+            original_message = await self.get_thread_original_message(payload.thread)
+            full_message = await payload.thread.parent.fetch_message(original_message.id)
+
+            for reaction in full_message.reactions:
+                if reaction.emoji == '✅':
+                    self._logger.info("Question has check emoji, let it archived")
+                    return
+
+            self._logger.info("Question do not have the check emoji, stay open")
+            await payload.thread.edit(archived=False)
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self._bot.user:
+            return
+
+        # if message.channel.id == 859433172704690186:
+        #     embed = discord.Embed(description=message.content, timestamp=message.created_at)
+        #     embed.set_author(name=f"{message.author}",
+        #                      icon_url=message.author.avatar.url)
+        #
+        #     bot_message = await message.channel.send(None, embed=embed)
+        #
+        #     report = await api_client.reports.send_report(DiscordMessage(
+        #         id=bot_message.id,
+        #         content=str(message.clean_content),
+        #         author=DiscordUser(message.author.id, message.author.name, message.author.discriminator),
+        #         created_at=message.created_at.timestamp()
+        #     ))
+        #
+        #     await bot_message.edit(view=PlayerReport(f"https://kanka.io/fr/campaign/93396/journals/{report['id']}"))
+        #
+        #     confirm = await message.channel.send(f'{message.author.mention}, merci pour ton rapport. Est-ce que tu '
+        #                                          f'souhaites indiquer un emplacement en lien sur la carte des joueurs ?',
+        #                                          view=PlayerReportConfirm())
+        #
+        #     await message.delete()
+
+        if message.channel.parent_id == self._config.discord.questions_channel and message.content == '✅':
+            self._logger.info(f"Question is considered answered, closing the thread")
+            await message.channel.send('Ce sujet est désormais clos, merci.')
+            self._logger.debug('Fetching original message')
+            original_message = await self.get_thread_original_message(message.channel)
+            self._logger.debug('Fetching full original message')
+            full_message = await message.channel.parent.fetch_message(original_message.id)
+            self._logger.debug('Adding reaction')
+            await full_message.add_reaction('✅')
+            self._logger.debug('Archiving thread')
+            await message.channel.edit(archived=True)
+
 
 class WorkaroundBot(commands.Bot):
 
@@ -99,39 +156,22 @@ class WorkaroundBot(commands.Bot):
         intents.message_content = True
         intents.guilds = True
         self._config = config
+        self._logger = logging.getLogger('bot')
+        self._cog = WorkaroundCog(config, self)
 
         super().__init__(command_prefix=commands.when_mentioned, intents=intents, **options)
 
     async def on_ready(self):
-        print(f'We have logged in as {self.user}')
+        self._logger.info(f'We have logged in as {self.user}')
 
     async def setup_hook(self) -> None:
-        await self.add_cog(WorkaroundCog(self._config))
+        await self.add_cog(self._cog)
 
-    # async def on_message(self, message: discord.Message):
-    #     if message.author != self.user:
-    #         pass
-    # if message.channel.id == 859433172704690186:
-    #     embed = discord.Embed(description=message.content, timestamp=message.created_at)
-    #     embed.set_author(name=f"{message.author}",
-    #                      icon_url=message.author.avatar.url)
-    #
-    #     bot_message = await message.channel.send(None, embed=embed)
-    #
-    #     report = await api_client.reports.send_report(DiscordMessage(
-    #         id=bot_message.id,
-    #         content=str(message.clean_content),
-    #         author=DiscordUser(message.author.id, message.author.name, message.author.discriminator),
-    #         created_at=message.created_at.timestamp()
-    #     ))
-    #
-    #     await bot_message.edit(view=PlayerReport(f"https://kanka.io/fr/campaign/93396/journals/{report['id']}"))
-    #
-    #     confirm = await message.channel.send(f'{message.author.mention}, merci pour ton rapport. Est-ce que tu '
-    #                                          f'souhaites indiquer un emplacement en lien sur la carte des joueurs ?',
-    #                                          view=PlayerReportConfirm())
-    #
-    #     await message.delete()
+    async def on_message(self, message: discord.Message):
+        await self._cog.on_message(message)
+
+    async def on_raw_thread_update(self, payload: discord.RawThreadUpdateEvent):
+        await self._cog.on_raw_thread_update(payload)
 
 
 def main():
@@ -141,6 +181,9 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
     intents.guilds = True
+
+    # Ugly SN to dict converted
+    logging.config.dictConfig(json.loads(json.dumps(config.log, default=lambda x: vars(x))))
 
     bot = WorkaroundBot(config)
 
