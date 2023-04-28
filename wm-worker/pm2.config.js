a01cf788cb043a2a0c01bcafd0a08d72b89f7969
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-04-28 16:54:01 +0200

    feat: randomly announce date on calendar update

--- wm-worker/pm2.config.js
+++ wm-worker/pm2.config.js
@@ -3,8 +3,8 @@ module.exports = {
     name: "worker",
     interpreter: "python",
     script: "/opt/project/wm-worker/src/__main__.py",
-//    interpreter_args : "-m debugpy --listen 0.0.0.0:5678", // VScode
-//     interpreter_args : "/usr/local/lib/python3.10/site-packages/pydevd.py --port 5680 --client 172.17.0.1 --file", // PyCharm
+    // interpreter_args : "-m debugpy --listen 0.0.0.0:5678", // VScode
+    // interpreter_args : "/usr/local/lib/python3.10/site-packages/pydevd.py --port 5680 --client 172.17.0.1 --file", // PyCharm
     watch: "/opt/project/wm-worker/src"
   }]
 }
--- wm-worker/src/__main__.py
+++ wm-worker/src/__main__.py
@@ -1,3 +1,5 @@
+import random
+
 import aiocron
 import argparse
 import asyncio
@@ -64,13 +66,14 @@ class Kanka:
         self.queue = queue
         self.discord = discord
         self.discord_notification_channel = None
+        self.discord_calendar_notification_channels = None
         self.logger = logging.getLogger('kanka')
 
         self.init_cache()
 
     def fetch(self, endpoint, last_sync=None, page=1):
         data = []
-        params = {"page": page}
+        params = {"page": page, "related": 1}
 
         if last_sync:
             params["lastSync"] = last_sync
@@ -85,7 +88,7 @@ class Kanka:
         if resp.status_code == 200:
             self.logger.debug(f'GET {resp.url} - {resp.status_code}')
             resp_json = resp.json()
-            data += resp_json['data']
+            data += resp_json['data'] if isinstance(resp_json['data'], list) else [resp_json['data']]
             if resp_json['links']['next']:
                 query = urlparse(resp_json['links']['next']).query
                 if query:
@@ -107,6 +110,16 @@ class Kanka:
             self.discord_notification_channel = await self.discord.fetch_channel(self.config.discord.kanka_notify_channel)
         return self.discord_notification_channel
 
+    async def get_calendar_notification_config(self):
+        channels = []
+
+        if not self.discord_calendar_notification_channels:
+            for _id in self.config.discord.kanka_calendar_notification_channels:
+                channels.append(await self.discord.fetch_channel(_id))
+            self.discord_calendar_notification_channels = channels
+
+        return self.config.kanka.calendar_entity_id, self.discord_calendar_notification_channels
+
     async def get_users(self):
         if not self.user_cache:
             cache = {}
@@ -118,6 +131,13 @@ class Kanka:
             self.user_cache = cache
         return self.user_cache
 
+    def get_date_announce(self, day, month, year):
+        return random.choice(self.config.discord.random.date_announce) % {'day': day, 'month': month, 'year': year}
+
+    def fetch_entity(self, _id: int):
+        [entity], __ = self.fetch(self.config.kanka.api_endpoint + f'/entities/{_id}')
+        return entity
+
     def fetch_entities(self):
         return self.fetch(self.config.kanka.api_endpoint + '/entities', self.entities_cache["last_sync"])
 
@@ -128,7 +148,7 @@ class Kanka:
         print('[Kanka] syncing')
         entities, last_sync = self.fetch_entities()
         self.entities_cache["last_sync"] = last_sync
-        print(f'[Kanka] sync ok, entities={len(entities)}, lastSync={self.entities_cache["last_sync"]}')
+        self.logger.info(f'[Kanka] sync ok, entities={len(entities)}, lastSync={self.entities_cache["last_sync"]}')
 
         await self.notify(entities)
 
@@ -143,6 +163,8 @@ class Kanka:
 
     async def notify(self, entities):
         channel = await self.get_notification_channel()
+        calendar_id, calendar_channels = await self.get_calendar_notification_config()
+
         count = len(entities)
         if count > 100:
             self.logger.warning(f'{count} entities modified, no notifications')
@@ -151,6 +173,21 @@ class Kanka:
                 author = await self.get_user(entity["updated_by"])
                 await channel.send(f'{author or "Inconnu"} a modifié "{entity["name"]}"\n'
                                    f'{self.get_entity_url(entity)}')
+
+                if entity['id'] == calendar_id:
+                    try:
+                        entity = self.fetch_entity(entity['id'])
+                        months = entity['child']['months']
+                        [cal_year, cal_month, cal_day] = entity['child']['date'].split('-')
+
+                        for calendar_chanel in calendar_channels:
+                            await calendar_chanel.send(
+                                self.get_date_announce(cal_day, months[int(cal_month) - 1]["name"], cal_year)
+                            )
+                    except Exception as e:
+                        self.logger.exception(f'[Kanka] failed to send date notification: {e.__class__.__name__} {e}',
+                                              exc_info=False)
+                break
 
     def init_cache(self):
         users_cache_file = Path(self.config.cache.base_path) / 'users.cache.json'
