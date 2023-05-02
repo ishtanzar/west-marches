3e0e3073efecc01cb7a9171ab8cd65ccd740ee87
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-05-02 22:40:58 +0200

    feat: specific access logs for /api/users/*

--- foundry-extensible-plugin/plugins/accessLogs/main.mjs
+++ foundry-extensible-plugin/plugins/accessLogs/main.mjs
@@ -11,11 +11,19 @@ export default class AccessLogsPlugin {
             write: (message) => global.logger.http(message)
         };
 
+        morgan.token('req-headers', (req, res) => JSON.stringify(req.headers))
+        morgan.token('req-body', (req, res) => JSON.stringify(req.body))
+
         base.hooks.on('pre.express.middleware', router => {
             router.use(morgan(
                 ':remote-addr ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
                 {stream: stream }
             ));
+
+            router.use('/api/users/*', morgan(
+                'req.headers=:req-headers, req.body=:req-body, resp.body=:res[body]',
+                {stream: stream }
+            ))
         });
     }
 }
