fbf582473e0a5554356be0da10f90842918fe133
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-09-17 08:29:13 +0200

    feat: add queue/job from wm-worker

--- wm-utils/setup.py
+++ wm-utils/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='wm-utils',
-    version='0.0.1',
+    version='0.0.2-dev',
     install_requires=[
-        'requests==2.31.0'
+        'requests>=2.31.0,<3.0',
+        'redis[hiredis]>=4.6.0,<5.0',
     ],
     packages=find_packages(
         where='src'
