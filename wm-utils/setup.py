4c484c179b10c4268c851310b0a2c26002d50dcb
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-09-17 08:29:48 +0200

    release wm-utils/0.0.2

--- wm-utils/setup.py
+++ wm-utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wm-utils',
-    version='0.0.2-dev',
+    version='0.0.2',
     install_requires=[
         'requests>=2.31.0,<3.0',
         'redis[hiredis]>=4.6.0,<5.0',
