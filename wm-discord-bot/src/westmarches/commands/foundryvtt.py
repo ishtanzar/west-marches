7b05ea8ca8bd0c258f445fde982af3cacc239bfc
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-09-19 19:24:09 +0200

    feat: delayed & cancelable restart & backup

--- wm-discord-bot/src/westmarches/commands/foundryvtt.py
+++ wm-discord-bot/src/westmarches/commands/foundryvtt.py
@@ -1,20 +1,30 @@
+from asyncio import Task
+
+import asyncio
 import logging
 import os
 from abc import abstractmethod
-from discord import Member
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from discord import Member, TextChannel
 from discord.ext.commands import Context
 from redbot.core import commands, checks
-from typing import Optional
-from westmarches_utils.queue import Queue, JobDefinition
+from typing import Optional, Union, List
+from westmarches_utils.api import HTTPException
 from westmarches_utils.config import Config
+from westmarches_utils.queue import Queue, JobDefinition
 
 from westmarches.utils import CompositeMetaClass, MixinMeta
-from westmarches_utils.api import HTTPException
 
 log = logging.getLogger("red.westmarches.foundry")
 
 
 class FoundryCommands(MixinMeta, metaclass=CompositeMetaClass):
+
+    def __init__(self, **kwargs):
+        self._tasks_backups: set[Task] = set()
+        self._tasks_restart: set[Task] = set()
+        self._delayed_restart_handle = None
 
     @abstractmethod
     async def discord_api_wrapper(self, ctx: Context, messages_key: str, f):
@@ -107,23 +117,85 @@ class FoundryCommands(MixinMeta, metaclass=CompositeMetaClass):
                 await ctx.send(messages['foundry.activity.no_users'])
 
     @command_foundry.command(name="restart")
-    async def command_restart(self, ctx: commands.Context):
+    async def command_restart(self, ctx: commands.Context, *args: Union[TextChannel, int, str, None]):
         """Restart FoundryVTT"""
-        await self.discord_api_wrapper(ctx, 'foundry.restart', lambda: self.wm_api.management.foundry.restart())
+        channel: Optional[TextChannel] = None
+        delay: Optional[int] = 0
+        trigger = datetime.now()
 
-    @command_foundry.group(name="backup", invoke_without_command=True)
-    async def foundry_backup(self, ctx: commands.Context, schemas: Optional[str]):
+        for arg in args:
+            if isinstance(arg, int):
+                delay = arg
+            elif isinstance(arg, TextChannel):
+                channel = arg
+            elif isinstance(arg, str):
+                if arg == 'cancel':
+                    self._tasks_restart.pop().cancel()
+                    return
+
+        if delay:
+            delta = relativedelta(minutes=delay)
+            trigger += delta
+        else:
+            delay = 0
+
+        if channel:
+            async with self.config.messages() as messages:
+                await channel.send(messages['foundry.restart.notify'] % int(round(trigger.timestamp())))
+
+        await ctx.message.add_reaction('\U0001F4C5')
+
+        async def _do_restart():
+            await asyncio.sleep(delay * 60)
+            await ctx.message.add_reaction('\U000025B6')
+            # await self.discord_api_wrapper(ctx, 'foundry.restart', lambda: self.wm_api.management.foundry.restart())
+            await ctx.message.add_reaction('\U00002705')
+
+        task = asyncio.create_task(_do_restart())
+        self._tasks_restart.add(task)
+        task.add_done_callback(self._tasks_restart.discard)
+
+    @command_foundry.command(name="backup", invoke_without_command=True)
+    async def foundry_backup(self, ctx: commands.Context, *args: List[Union[TextChannel, int, str, None]]):
         """Perform a backup of FoundryVTT. Beware that Foundry WILL BE STOPPED"""
+        schemas: Optional[str] = None
+        channel: Optional[TextChannel] = None
+        delay: Optional[int] = 0
+        trigger = datetime.now()
+
+        for arg in args:
+            if isinstance(arg, int):
+                delay = arg
+            elif isinstance(arg, TextChannel):
+                channel = arg
+            elif isinstance(arg, str):
+                if arg == 'cancel':
+                    self._tasks_backups.pop().cancel()
+                    return
+                schemas = arg
+
+        if delay:
+            delta = relativedelta(minutes=delay)
+            trigger += delta
 
         config = Config()
         config.set('redis.endpoint', await self.config.redis.endpoint())
-
         queue = Queue(config)
-        await queue.put(JobDefinition('foundry.backup',
-                                      schemas=[s.strip() for s in schemas.split(',')] if schemas else ['worlds'],
-                                      reaction_msg={
-                                          'channel_id': ctx.channel.id,
-                                          'message_id': ctx.message.id
-                                      }))
 
-        await ctx.message.add_reaction('\U0001F4C5')
+        if channel:
+            async with self.config.messages() as messages:
+                await channel.send(messages['foundry.restart.notify'] % int(round(trigger.timestamp())))
+
+        async def _do_backup():
+            await asyncio.sleep(delay * 60)
+            await queue.put(JobDefinition('foundry.backup',
+                                          schemas=[s.strip() for s in schemas.split(',')] if schemas else ['worlds'],
+                                          reaction_msg={
+                                              'channel_id': ctx.channel.id,
+                                              'message_id': ctx.message.id
+                                          }))
+            await ctx.message.add_reaction('\U0001F4C5')
+
+        task = asyncio.create_task(_do_backup())
+        self._tasks_backups.add(task)
+        task.add_done_callback(self._tasks_backups.discard)
