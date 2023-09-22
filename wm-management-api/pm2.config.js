fd65e45029a3c275e9e70d7e65f6c0ef23ad8b37
ishtanzar <82805080+ishtanzar@users.noreply.github.com>
2023-09-22 11:08:00 +0200

    chore: dev files cleanup

--- wm-management-api/pm2.config.js
+++ wm-management-api/pm2.config.js
@@ -5,6 +5,6 @@ module.exports = {
     script: "/opt/project/wm-management-api/src/__main__.py",
     // interpreter_args : "-m debugpy --listen 0.0.0.0:5678", // VScode
     // interpreter_args : "/usr/local/lib/python3.11/site-packages/pydevd.py --port 5678 --client 172.17.0.1 --file", // PyCharm
-    watch: "/opt/project/wm-management-api/src"
+    watch: ["/opt/project/wm-management-api/src", "/opt/project/wm-utils/src"]
   }]
 }
